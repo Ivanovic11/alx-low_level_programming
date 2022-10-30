c57409e0de8a0cdb78f9dc076d95df35c8711774
Ivanovic11 <ivanigwe@gmail.com>
2022-10-30 10:38:11 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _isupper - prints uppercase characters
+ * @c: character to be printed
+ *
+ *Return: 0 if lowercase 1 if uppercase
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+welcome to more functions
