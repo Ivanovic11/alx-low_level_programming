6765cd78ecb1a6aebc13ee2bbec46dcd33618051
Ivanovic11 <ivanigwe@gmail.com>
2022-11-04 09:12:21 -0700

    task 0

---
+++ 0x0A-argc_argv/0-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+\#include <stdlib.h>
+
+/**
+ * main - check the code
+ * @argc: argument count
+ * @argv: argument value
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+	exit(EXIT_SUCCESS);
+}
---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - code runs here
+ * @argc: argument count
+ * @argv: argument value
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
