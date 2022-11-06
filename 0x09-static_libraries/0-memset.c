2cda2224f6d3db99b248f4a92b1a162edbc45422
Ivanovic11 <ivanigwe@gmail.com>
2022-11-06 13:31:58 -0800

    task 0

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,14 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+
 /**
-  * _memset - fills the first n bytes of the memory areay pointed to
-  *by s with the constant byte b
-  *@s: the string s pointer
-  *@b: the constant byte b
-  *@n: thr number of bytes to be filled
-  *Return: pointer to char
-  */
+ * _memset - fills the first n bytes of the memory array pointed
+   to by s with the constant byte b
+ * @s: the string's pointert
+ * @b: the constant byte b
+ * @n: the number of bytes to be filled
+ * Return: pointer to char
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	memset(s, b, n);
